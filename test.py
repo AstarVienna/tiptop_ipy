3ffc0a7ba3a5f521a89d7804b67ab42feaa2bac2
Martin Baláž <martin.balaz@trojsten.sk>
2023-11-21 16:43:05 +0000

    New version of scalyca

--- test.py
+++ test.py
@@ -2,28 +2,30 @@
 # It would be super cool to spend time getting the configuration files to work
 import argparse
 import argparsedirs
+import logging
 
 from pathlib import Path
 from scalyca import Scala
 from tiptop_ipy.tiptop_connection import TipTopConnection
-import logging
-
+import scalyca.colour as c
 
 log = logging.getLogger('root')
-log.setLevel(logging.DEBUG)
+
 
 class TipTop(Scala):
-    app_name = "TIPTOP wrapper"
-    prog = ""
+    _app_name = "TipTop wrapper"
+    _prog = "TipTop wrapper"
 
     def add_arguments(self):
-        self.argparser.add_argument('template', type=argparse.FileType('r'))
-        self.argparser.add_argument('outdir', action=argparsedirs.WriteableDir)
+        self.add_argument('template', type=argparse.FileType('r'))
+        self.add_argument('outdir', action=argparsedirs.WriteableDir)
 
     def main(self):
+        log.info(f"{self._prog} starting")
         connection = TipTopConnection(self.args.template)
+        log.info(f"Making a request with config file {c.path(self.args.template.name)}")
         hdu = connection.query_server()
-        connection.writeto(Path(self.args.outdir) / 'tiptop.fits')
+        connection.writeto(Path(self.args.outdir) / 'tiptop.fits', overwrite=True)
 
 
 if __name__ == "__main__":
