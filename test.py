86ad7f581892cea48fe6aafad43bc8977805b15e
Martin Baláž <martin.balaz@trojsten.sk>
2023-11-23 16:55:32 +0000

    Added Schema

--- test.py
+++ test.py
@@ -3,22 +3,132 @@
 import argparse
 import argparsedirs
 import logging
+import configparser
 
+from ast import literal_eval
 from pathlib import Path
 from scalyca import Scala
 from tiptop_ipy.tiptop_connection import TipTopConnection
+from schema import Schema, Or, And, Optional, SchemaError
 import scalyca.colour as c
 
 log = logging.getLogger('root')
 
 
+
+class IniParser(configparser.ConfigParser):
+    optionxform = str
+    _converters = {
+        'any': lambda x: literal_eval(x)
+    }
+
+    def as_dict(self):
+        d = dict(self._sections)
+        for k in d:
+            d[k] = dict(self._defaults, **d[k])
+            d[k].pop('__name__', None)
+            for sk in d[k]:
+                d[k][sk] = literal_eval(d[k][sk])
+        return d
+
+
+number = Or(int, float)
+
+
 class TipTop(Scala):
-    _app_name = "TipTop wrapper"
-    _prog = "TipTop wrapper"
+    _app_name = "TipTop ipy"
+    _prog = "TipTop ipy"
+
+    _schema = Schema({
+        'telescope': {
+            'TelescopeDiameter': number,
+            Optional('ObscurationRatio'): number,
+            'Resolution': int,
+            Optional('ZenithAngle'): number,
+            Optional('TechnicalFoV'): number,
+            Optional('PathPupil'): Or(argparse.FileType('r'), ''),
+            Optional('PathStaticOn'): Or(argparse.FileType('r'), ''),
+            Optional('PathStaticOff'): Or(argparse.FileType('r'), ''),
+            Optional('PathStaticPos'): Or(argparse.FileType('r'), ''),
+            'PupilAngle': number,
+            Optional('PathApodizer'): Or(argparse.FileType('r'), ''),
+            Optional('PathStatModes'): Or(argparse.FileType('r'), ''),
+        },
+        'atmosphere': {
+            'Wavelength': number,
+            'Seeing': number,
+            'L0': number,
+            'Cn2Weights': [number],
+            'Cn2Heights': [number],
+            'WindSpeed': [number],
+            'WindDirection': [number],
+            'r0_Value': number,
+            'testWindspeed': number,
+        },
+        'sources_science': {
+            'Wavelength': [number],
+            'Zenith': [number],
+            'Azimuth': [number],
+        },
+        'sources_HO': {
+            'Wavelength': number,
+            'Zenith': [number],
+            'Azimuth': [number],
+            'Height': number,
+        },
+        'sensor_science': {
+            'PixelScale': number,
+            'FieldOfView': number,
+        },
+        'sensor_HO': {
+            'WfsType': str,
+            'Modulation': number,
+            'PixelScale': number,
+            'FieldOfView': number,
+            'Binning': number,
+            'NumberPhotons': [int],
+            'SigmaRON': number,
+            'Dark': number,
+            'SkyBackground': number,
+            'Gain': number,
+            'ExcessNoiseFactor': number,
+            'NumberLenslets': [int],
+            'SizeLenslets': [number],
+            'NoiseVariance': [Or(None, number)],
+        },
+        'DM': {
+            'NumberActuators': [number],
+            'DmPitchs': [number],
+            Optional('InfModel'): str,
+            Optional('InfCoupling'): [number],
+            Optional('DmHeights'): [number],
+            Optional('OptimizationZenith'): [number],
+            Optional('OptimizationAzimuth'): [number],
+            Optional('OptimizationWeight'): [number],
+            Optional('OptimizationConditioning'): number,
+            Optional('NumberReconstructedLayers'): int,
+            Optional('AoArea'): 'circle',
+        },
+        'RTC': {
+            'LoopGain_HO': number,
+            'SensorFrameRate_HO': number,
+            'LoopDelaySteps_HO': int,
+        },
+    })
 
     def add_arguments(self):
         self.add_argument('template', type=argparse.FileType('r'))
         self.add_argument('outdir', action=argparsedirs.WriteableDir)
+
+    def initialize(self):
+        parser = IniParser()
+        parser.read(self.args.template.name)
+        try:
+            self._schema.validate(parser.as_dict())
+            log.info(f"Successfully validated template file {c.path(self.args.template.name)} against schema")
+        except SchemaError as e:
+            log.error(f"Could not validate template tile {c.path(self.args.template.name)}!")
+            raise e
 
     def main(self):
         connection = TipTopConnection(self.args.template)
